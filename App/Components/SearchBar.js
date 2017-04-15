e4f1d6ecd2f765b50101572824c2f8f69a551fe1
loiralae <h67zhou@uwaterloo.ca>
2017-04-15 18:26:34 -0400

    Modify search bar

--- App/Components/SearchBar.js
+++ App/Components/SearchBar.js
@@ -8,6 +8,11 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default class SearchBar extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { searchTerm: '' };
+  }
+
   static propTypes = {
     onSearch: React.PropTypes.func.isRequired,
     onCancel: React.PropTypes.func.isRequired,
@@ -16,19 +21,19 @@ export default class SearchBar extends React.Component {
 
   render () {
     const { onSearch, onCancel, searchTerm } = this.props
-    const onSubmitEditing = () => onSearch(searchTerm)
+    const onSubmitEditing = () => onSearch(this.state.searchTerm)
     return (
       <Animatable.View animation='slideInRight' duration={250} style={styles.container}>
         <Icon name='search' size={Metrics.icons.tiny} style={styles.searchIcon} />
         <TextInput
           ref='searchText'
           autoFocus
-          placeholder={I18n.t('search')}
+          placeholder='What ingredients do you have?'
           placeholderTextColor={Colors.snow}
           underlineColorAndroid='transparent'
           style={styles.searchInput}
           value={this.props.searchTerm}
-          onChangeText={onSearch}
+          onChangeText={(searchTerm) => this.setState({searchTerm})}
           autoCapitalize='none'
           onSubmitEditing={onSubmitEditing}
           returnKeyType={'search'}
