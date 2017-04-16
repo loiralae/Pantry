d5078b154e77f071c925388157e94feb4465d3b4
Kushal Kumarasinghe <kushal.k@hotmail.com>
2017-04-15 20:07:53 -0400

    Recipe view page

---
+++ App/Containers/RecipeView.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { TabViewAnimated, TabBar } from 'react-native-tab-view';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class TabViewExample extends Component {
+  state = {
+    index: 0,
+    routes: [
+      { key: '1', title: 'Ingredients' },
+      { key: '2', title: 'Recipe' },
+    ],
+  };
+
+  _handleChangeTab = (index) => {
+    this.setState({ index });
+  };
+
+  _renderHeader = (props) => {
+    return <TabBar {...props} />;
+  };
+
+  _renderScene = ({ route }) => {
+    switch (route.key) {
+    case '1':
+      return <View style={[ styles.page, { backgroundColor: '#ff4081' } ]} />;
+    case '2':
+      return <View style={[ styles.page, { backgroundColor: '#673ab7' } ]} />;
+    default:
+      return null;
+    }
+  };
+
+  render() {
+    var tabview = <TabViewAnimated
+        style={styles.container}
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onRequestChangeTab={this._handleChangeTab}
+      />
+    return (
+      tabview
+    );
+  }
+}
